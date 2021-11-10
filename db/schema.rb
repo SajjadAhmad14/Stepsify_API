94f10d583d34acbe46dbdbbdc0353be6104786c9
Sajjad Ahmad <jogimar14@gmail.com>
2021-11-10 14:47:24 +0500

    Generate activity model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_092800) do
+ActiveRecord::Schema.define(version: 2021_11_10_094404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.decimal "target"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
