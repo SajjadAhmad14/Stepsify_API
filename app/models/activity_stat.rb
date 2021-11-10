2253a539aecd5ce7b6f5a9f40ba8714218f11de1
Sajjad Ahmad <jogimar14@gmail.com>
2021-11-10 14:51:24 +0500

    Generate activity_stats model

---
+++ app/models/activity_stat.rb
@@ -0,0 +1,2 @@
+class ActivityStat < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_094404) do
+ActiveRecord::Schema.define(version: 2021_11_10_094833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "activities", force: :cascade do |t|
     t.decimal "target"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "activity_stats", force: :cascade do |t|
+    t.decimal "steps"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
