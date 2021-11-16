ce5735df5fad14d7b8002b0f32486f286d2c7191
Sajjad Ahmad <jogimar14@gmail.com>
2021-11-16 14:00:52 +0500

    Add height and weight attributes to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :activities, dependent: :destroy
   has_many :activity_stats, dependent: :destroy
   validates :username, uniqueness: true
+  validates :username, :password, :sex, :height, :weight, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_094833) do
+ActiveRecord::Schema.define(version: 2021_11_12_124801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2021_11_10_094833) do
     t.string "sex"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "height"
+    t.decimal "weight"
   end
 
 end
